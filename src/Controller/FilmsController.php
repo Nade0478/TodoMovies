f4b5673d1502f6f75f1a28fef8be7eea8fa293a3
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-01-20 23:35:36 +0100

    subject

--- src/Controller/FilmsController.php
+++ src/Controller/FilmsController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\String\Slugger\SluggerInterface; 
+use Symfony\Component\Form\Extension\Core\Type\FileType; 
+use Symfony\Component\Validator\Constraints\File; 
 
 #[Route('/films')]
 final class FilmsController extends AbstractController
